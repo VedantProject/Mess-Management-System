d77dcf99045354d8b182678db71e71759d0960b0
VedantProject <kasatvedant8976@gmail.com>
2025-10-04 20:27:32 +0530

    refactor: Remove core backend directories

--- backend/middleware/roleMiddleware.js
+++
@@ -1,6 +0,0 @@
-export const staffOnly = (req, res, next) => {
-  if (req.user?.role !== 'Staff') {
-    return res.status(403).json({message: 'Staff access only'});
-  }
-  next();
-};
--- backend/utils/generateToken.js
+++
@@ -1,4 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const signAuthToken = (payload, secret, expiresIn) =>
-    jwt.sign(payload, secret, {expiresIn});
