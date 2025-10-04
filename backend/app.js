086a355b61e1dac6959f9a82d90feca0f61e59dd
VedantProject <kasatvedant8976@gmail.com>
2025-10-04 18:12:26 +0530

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
-const express=require('express');
-const dotenv=require('dotenv');
-const connectDB=require('./config/db')
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes');
 dotenv.config();
 connectDB()
-const app=express();
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use('/api/users', userRoutes)
+const PORT = process.env.PORT || 1934
+app.listen(PORT, () => console.log(`Server Started at PORT ${PORT} `))
